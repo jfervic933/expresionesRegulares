4f31aa38777c1dfe7e7b84c4d288961d2cd819b7
jcarlosvico <jfervic933@maralboran.es>
2023-02-23 17:04:31 +0100

    Expresiones regulares

---
+++ src/main/java/maralboran/expresionesregulares/DNI.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package maralboran.expresionesregulares;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author carlos
+ */
+public class DNI {
+    
+    public static void main(String[] args) {
+        String dni = "73552299D";
+        String regex = "^[0-9]{8}[a-zA-Z]$"; // También se puede \d{8}[a-zA-Z]
+
+        boolean isValidDNI = dni.matches(regex);
+        System.out.println(isValidDNI);
+        
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(dni);
+        
+        System.out.println("Patrones encontrados en la cadena email");
+        while (matcher.find()) {
+            System.out.println("Patrón encontrado: " + matcher.group());
+        }
+
+    }
+    
+}
